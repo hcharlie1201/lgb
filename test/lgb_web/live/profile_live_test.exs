85a0fc2f7ba4eb812dfee084a964ad3fc37d6fd8
Charlie Wu <hcharlie1201@gmail.com>
2025-01-15 17:18:08 -0800

    style: Reformat profile live test attributes for improved readability

--- test/lgb_web/live/profile_live_test.exs
+++ test/lgb_web/live/profile_live_test.exs
@@ -5,13 +5,40 @@ defmodule LgbWeb.ProfileLiveTest do
   import Lgb.AccountsFixtures
   import Lgb.ProfilesFixtures
 
-  @create_attrs %{handle: "some handle", state: "some state", zip: "some zip", dob: "2024-12-11", height_cm: 42, weight_lb: 42, city: "some city", biography: "some biography"}
-  @update_attrs %{handle: "some updated handle", state: "some updated state", zip: "some updated zip", dob: "2024-12-12", height_cm: 43, weight_lb: 43, city: "some updated city", biography: "some updated biography"}
-  @invalid_attrs %{handle: nil, state: nil, zip: nil, dob: nil, height_cm: nil, weight_lb: nil, city: nil, biography: nil}
+  @create_attrs %{
+    handle: "some handle",
+    state: "some state",
+    zip: "some zip",
+    dob: "2024-12-11",
+    height_cm: 42,
+    weight_lb: 42,
+    city: "some city",
+    biography: "some biography"
+  }
+  @update_attrs %{
+    handle: "some updated handle",
+    state: "some updated state",
+    zip: "some updated zip",
+    dob: "2024-12-12",
+    height_cm: 43,
+    weight_lb: 43,
+    city: "some updated city",
+    biography: "some updated biography"
+  }
+  @invalid_attrs %{
+    handle: nil,
+    state: nil,
+    zip: nil,
+    dob: nil,
+    height_cm: nil,
+    weight_lb: nil,
+    city: nil,
+    biography: nil
+  }
 
   setup do
     user = user_fixture()
-    profile = profile_fixture(%{user_id: user.id})
+    profile = profile_fixture(user)
     %{user: user, profile: profile}
   end
 
