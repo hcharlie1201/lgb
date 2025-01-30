b7adf63250ca36d9779ccdd63584717638e6ebfd
Charlie Wu <hcharlie1201@gmail.com>
2025-01-30 09:56:45 -0800

    fix deploy

--- priv/repo/migrations/20241209035617_create_messages.exs
+++ priv/repo/migrations/20241209035617_create_messages.exs
@@ -3,14 +3,14 @@ defmodule Lgb.Repo.Migrations.CreateMessages do
 
   def change do
     create table(:messages) do
-      add :profile_id, references(:profiles, on_delete: :nothing)
+      add :user_id, references(:users, on_delete: :nothing)
       add :chat_room_id, references(:chat_rooms, on_delete: :nothing)
       add :content, :string
 
       timestamps(type: :utc_datetime)
     end
 
-    create index(:messages, [:profile_id])
+    create index(:messages, [:user_id])
     create index(:messages, [:chat_room_id])
   end
 end
