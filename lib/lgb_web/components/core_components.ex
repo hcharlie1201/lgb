22ad6d364e0977e5693529b26d47a3f7caa35b0b
Macbook Pr0 <macbookpr0@Macbooks-MacBook-Pro.local>
2025-02-02 16:01:51 -0800

    more changes

--- lib/lgb_web/components/core_components.ex
+++ lib/lgb_web/components/core_components.ex
@@ -481,7 +481,7 @@ defmodule LgbWeb.CoreComponents do
 
     ~H"""
     <div class="m-4 p-0.5 overflow-y-auto px-4 sm:overflow-visible sm:px-0">
-      <table class="w-[40rem] mt-11 sm:w-full">
+      <table class="w-[40rem] mt-1 sm:w-full">
         <thead class="text-sm text-left leading-6 text-zinc-500">
           <tr>
             <th :for={col <- @col} class="p-0 pb-4 pr-6 font-normal">{col[:label]}</th>
@@ -733,7 +733,7 @@ defmodule LgbWeb.CoreComponents do
               d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
             />
           </svg>
-          <span class="text-xs mt-1">Profile</span>
+          <span class="userNavText text-xs mt-1">Profile</span>
         </.link>
         <.link
           class="flex flex-col items-center p-4 text-gray-700 hover:bg-purple-50 hover:text-colorSecondary rounded-lg transition-colors duration-200"
@@ -751,7 +751,7 @@ defmodule LgbWeb.CoreComponents do
               clip-rule="evenodd"
             />
           </svg>
-          <span class="text-xs mt-1">Search</span>
+          <span class="userNavText text-xs mt-1">Search</span>
         </.link>
         <.link
           class="flex flex-col items-center p-4 text-gray-700 hover:bg-purple-50 hover:text-colorSecondary rounded-lg transition-colors duration-200"
@@ -771,7 +771,7 @@ defmodule LgbWeb.CoreComponents do
               d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 6 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
             />
           </svg>
-          <span class="text-xs mt-1">Inbox</span>
+          <span class="userNavText text-xs mt-1">Inbox</span>
         </.link>
         <.link
           class="flex flex-col items-center p-4 text-gray-700 hover:bg-purple-50 hover:text-colorSecondary rounded-lg transition-colors duration-200"
@@ -791,7 +791,7 @@ defmodule LgbWeb.CoreComponents do
               d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
             />
           </svg>
-          <span class="text-xs mt-1">Chatroom</span>
+          <span class="userNavText text-xs mt-1">Chatroom</span>
         </.link>
         <.link
           class="flex flex-col items-center p-4 text-gray-700 hover:bg-purple-50 hover:text-colorSecondary rounded-lg transition-colors duration-200"
@@ -811,7 +811,7 @@ defmodule LgbWeb.CoreComponents do
               d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
             />
           </svg>
-          <span class="text-xs mt-1">Premium</span>
+          <span class="userNavText text-xs mt-1">Premium</span>
         </.link>
         <.link
           class="flex flex-col items-center p-4 text-gray-700 hover:bg-purple-50 hover:text-colorSecondary rounded-lg transition-colors duration-200"
@@ -831,7 +831,7 @@ defmodule LgbWeb.CoreComponents do
               d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3"
             />
           </svg>
-          <span class="text-xs mt-1">Account</span>
+          <span class="userNavText text-xs mt-1">Account</span>
         </.link>
       </nav>
       <!-- Main Content -->
